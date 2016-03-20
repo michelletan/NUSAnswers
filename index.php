c584444d50202753461619b846931b72fa210cf2
Michelle Tan <michelletanjm@gmail.com>
2016-03-20 15:50:39 +0800

    Added bootstrap files, php dispatch library, index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="en">
+
+<head>
+<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
+<title><?php echo title;?></title>
+<link href="css/bootstrap.css" rel="stylesheet">
+<link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+</body>
+</html>
