6169ae76c5eb06982520525038ecf233eb6f35ba
Michelle Tan <michelletanjm@gmail.com>
2016-03-24 14:31:00 +0800

    Removed ui folder

--- index.php
+++ index.php
@@ -1,18 +1,19 @@
-<?php include_once 'php/ui/header.php'; ?>
+<?php include_once 'php/header.php'; ?>
 <body>
-    <?php include_once 'php/ui/navbar.php'; ?>
+    <?php include_once 'php/navbar.php'; ?>
     <div class="container-fluid center-block">
         <div class="row">
-            <?php include_once 'php/ui/sidebar.php'; ?>
+            <?php include_once 'php/sidebar.php'; ?>
             <div class="col-md-6 col-lg-6">
-                <?php include 'php/ui/question_list_item.php'; ?>
-                <?php include 'php/ui/question_list_item.php'; ?>
-                <?php include 'php/ui/question_list_item.php'; ?>
-                <?php include 'php/ui/question_list_item.php'; ?>
-                <?php include 'php/ui/question_list_item.php'; ?>
-                <?php include 'php/ui/question_list_item.php'; ?>
+                <?php include 'php/question_list_item.php'; ?>
+                <?php include 'php/question_list_item.php'; ?>
+                <?php include 'php/question_list_item.php'; ?>
+                <?php include 'php/question_list_item.php'; ?>
+                <?php include 'php/question_list_item.php'; ?>
+                <?php include 'php/question_list_item.php'; ?>
+                <?php include 'php/question_list_item.php'; ?>
             </div>
         </div>
     </div>
 </body>
-<?php include_once 'php/ui/footer.php'; ?>
+<?php include_once 'php/footer.php'; ?>
