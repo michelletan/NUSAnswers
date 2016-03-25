08795778048b2839125aa73084e5e489096ad454
Michelle Tan <michelletanjm@gmail.com>
2016-03-25 15:26:44 +0800

    Removed ajax test

--- views/footer.php
+++ views/footer.php
@@ -1,11 +1,3 @@
 <script src="../js/jquery-1.12.2.js"></script>
 <script src="../js/bootstrap.js"></script>
-<script type="text/javascript">
-$.ajax({
-  url: "/json/something"
-}).success(function(data) {
-  alert(data);
-  console.log(data);
-});
-</script>
 </html>
