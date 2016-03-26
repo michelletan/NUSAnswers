0ebd45014379c598654e146ecdf536ed1d306b19
JosephineKwa <work.and.stuff.jo@gmail.com>
2016-03-26 14:13:31 +0800

    add admin create account pages

---
+++ views/admin_create_moderator_account.php
@@ -0,0 +1,28 @@
+<?php include_once __DIR__ . '/admin_header.php'; ?>
+<body>
+    <?php include_once __DIR__ . '/admin_navbar.php'; ?>
+    <div class="container-fluid">
+        <div class="row">
+            <?php include_once __DIR__ . '/admin_sidebar.php'; ?>
+            <div class="main col-md-9 col-lg-10">
+                <div class="top-buffer-20px panel panel-default">
+                    <ul class="list-group">
+                        <li class="list-group-item">
+                            <h4>Create Moderator Account</h4>
+                        </li>
+                        <li class="list-group-item summary-display">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <form>
+                                        <?php include_once __DIR__ . '/admin_create_account_form_inputs.php'; ?>
+                                    </form>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+<?php include_once __DIR__ . '/admin_footer.php'; ?>
