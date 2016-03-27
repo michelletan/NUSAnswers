7851ef22edde218dda1e33484597dff8616e025a
Michelle Tan <michelletanjm@gmail.com>
2016-03-27 23:25:24 +0800

    Updated styles for ask page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+$(document).ready(function() {
+  $('[data-toggle=offcanvas]').click(function() {
+    $('.row-offcanvas').toggleClass('active');
+  });
+});
 
 // Code for adding popup modal
 // $(document).ready(function() {
--- views/home.php
+++ views/home.php
@@ -3,9 +3,9 @@
     <script src="../js/fb.js"></script>
     <?php include_once __DIR__ . '/navbar.php'; ?>
     <div class="container-fluid center-block">
-        <div class="row">
+        <div class="row row-offcanvas row-offcanvas-left">
             <?php include_once __DIR__ . '/sidebar.php'; ?>
-            <div class="main col-md-6 col-lg-6">
+            <div class="main col-sm-9 col-md-6 col-lg-6">
                 <?php include __DIR__ . '/question_list_item.php'; ?>
                 <?php include __DIR__ . '/question_list_item.php'; ?>
                 <?php include __DIR__ . '/question_list_item.php'; ?>
