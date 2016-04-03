93e0b4b2be5a5b476eb999b52bafd7445ec61fc4
kawaiirice <quinnlert@193-81.priv23.nus.edu.sg>
2016-04-03 23:30:10 +0800

    Merge changes for recaptcha and image upload

---
+++ views/ask_success.php
@@ -0,0 +1,20 @@
+<?php include_once __DIR__ . '/header.php'; ?>
+<body>
+    <?php include_once __DIR__ . '/navbar.php'; ?>
+    <div class="container-fluid center-block">
+        <div class="row">
+            <?php include_once __DIR__ . '/sidebar.php'; ?>
+            <div class="main col-md-6 col-lg-6">
+                <h3 class="page-title">Post Your Question</h3>
+                <div class="card">
+                    <div class="post-content">
+                      <div class="post-answer">
+                        Question posted.            
+                      </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+<?php include_once __DIR__ . '/footer.php'; ?>
