00affc41fcb57fd9c7fae712063d4b549e26b913
Michelle Tan <michelletanjm@gmail.com>
2016-04-04 13:25:56 +0800

    Added analytics

--- views/footer.php
+++ views/footer.php
@@ -1,3 +1,4 @@
 <script src="../js/jquery-1.12.2.js"></script>
 <script src="../js/bootstrap.js"></script>
 <script src="../js/jquery.jscroll.js"></script>
+<?php //include_once("analyticstracking.php") ?>
