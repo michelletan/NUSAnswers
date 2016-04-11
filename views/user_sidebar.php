17748e48dd16b389a5d1df2c5f68407a443e1e36
Michelle Tan <michelletanjm@gmail.com>
2016-04-11 20:41:51 +0800

    Updated css for navbars

--- views/user_sidebar.php
+++ views/user_sidebar.php
@@ -2,8 +2,17 @@
 <nav id="mobile-menu" class="affix col-xs-5 col-sm-5 hidden-md hidden-lg hidden-xl">
     <div class="container-fluid center-block">
         <div class="row">
-            <div class="col-md-10 col-lg-10">
+            <div class="col-xs-6 col-sm-6 col-md-10 col-lg-10">
                 <div class="affix">
+                    <div class="card">
+                        <div class="sidebar-body">
+                            <form class= "navbar-left col-xs-12 col-sm-12 col-md-4 col-lg-4" role="search">
+                                <input type="text" id="nav-search-bar" class="form-control" placeholder="Search">
+                            </form>
+                            <a href="/ask/"><span class="glyphicon glyphicon-question-sign" aria-hidden="true"></span> Ask</a><br>
+                            <a href="/answer/"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span> Answer</a><br>
+                        </div>
+                    </div>
                     <ul class="metismenu nav" id="menu">
                         <li><a href="/user-dashboard"><span class="glyphicon glyphicon-stats"></span> Charts</a>
                         <li>
