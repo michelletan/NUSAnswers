99189b0ddab234e90cae5a9b81860646cbf46630
Michelle Tan <michelletanjm@gmail.com>
2016-04-11 15:44:26 +0800

    Updated user and admin dashboard navbars

--- views/user_sidebar.php
+++ views/user_sidebar.php
@@ -1,3 +1,39 @@
+<nav id="mobile-menu" class="affix col-xs-5 col-sm-5 hidden-md hidden-lg hidden-xl">
+    <div class="container-fluid center-block">
+        <div class="row">
+            <div class="col-md-10 col-lg-10">
+                <div class="affix">
+                    <ul class="metismenu nav" id="menu">
+                        <li><a href="/user-dashboard"><span class="glyphicon glyphicon-stats"></span> Charts</a>
+                        <li>
+                            <a href="#">
+                                <span class="glyphicon glyphicon-question-sign"></span>
+                                Questions
+                                 <span class="glyphicon arrow"></span>
+                            </a>
+                            <ul class="collapse nav">
+                                <li class=""><a href="/user-questions">View Questions</a></li>
+                                <li class=""><a href="/user-question-comments">View Questions Comments</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <span class="glyphicon glyphicon-comment"></span>
+                                Answers
+                                 <span class="glyphicon arrow"></span>
+                            </a>
+                            <ul class="collapse nav">
+                                <li class=""><a href="/user-answers">View Answers</a></li>
+                                <li class=""><a href="#">View Answers Comments</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
+
 <div class="sidebar col-md-3 col-lg-2">
     <div class="affix hidden-xs hidden-sm col-md-3 col-lg-2">
         <ul class="metismenu nav" id="menu">
