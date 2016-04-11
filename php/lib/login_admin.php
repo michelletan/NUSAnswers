e9cc6380fb6e51e9ebb634bc3a3aabf6476ed555
chowyb <chowwars@hotmail.com>
2016-04-11 18:34:15 +0800

    changed admin login to work with new naming

--- php/lib/login_admin.php
+++ php/lib/login_admin.php
@@ -1,18 +1,18 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/php/lib/dbaccess.php';
 
-function admin_login($admin_id_param, $password_param) {
+function admin_login($login_id_param, $password_param) {
   global $db;
-  $admin_id = $db->escape_string($admin_id_param);
+  $login_id = $db->escape_string($login_id_param);
   $password = $db->escape_string($password_param);
   $hashed_password = crypt($password, $password);
   $array_to_return = array();
-  $query = "SELECT * FROM admins WHERE admin_id='$admin_id'";
+  $query = "SELECT * FROM admins WHERE login_id='$login_id'";
   $result = $db->query($query);
   if ($row = $result->fetch_assoc()) {
     if ($hashed_password== $row['hashed_password']) {
       $array_to_return = array();
-      $array_to_return['admin-id'] = $row['admin_id'];
+      $array_to_return['login-id'] = $row['login_id'];
       $array_to_return['profile-id'] = $row['profile_fk'];
       $subquery = "SELECT display_name FROM profiles WHERE profile_id=" . $row['profile_fk'];
       $subresult = $db->query($subquery);
