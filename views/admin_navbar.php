9a0297ef9414a84e1ba226c9f6813fdc51c658e7
JosephineKwa <work.and.stuff.jo@gmail.com>
2016-04-20 14:20:29 +0800

    edit permission

--- views/admin_navbar.php
+++ views/admin_navbar.php
@@ -24,7 +24,7 @@
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?php echo get_active_display_name() ?> <span class="caret"></span></a>
                         <ul class="dropdown-menu">
-                            <?php if (has_admin_rights()) {?>
+                            <?php if (has_mod_rights()) {?>
                             <li><a href="/admin-dashboard">Admin Dashboard</a></li>
                             <?php } ?>
                             <li role="separator" class="divider"></li>
